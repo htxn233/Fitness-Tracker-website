c4adf8c8881f73261ac2c371fba24a4cfdfa4138
cuziycloud <tranmyvan6157@gmail.com>
2024-03-31 21:11:10 +0700

    Fix loder

--- assets/js/Login.js
+++ assets/js/Login.js
@@ -20,32 +20,3 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 	window.location.href = "index.html?email=" + email + "&password=" + password;
 });
 
-const leftPane = document.querySelector('.left-pane');
-const centerPane = document.querySelector('.center-pane');
-const rightPane = document.querySelector('.right-pane');
-
-// Thiết lập tốc độ di chuyển và khoảng cách giữa các ảnh
-const speed = 1; // Điều chỉnh tốc độ di chuyển ở đây
-const gap = 50; // Điều chỉnh khoảng cách giữa các ảnh ở đây
-
-// Hàm di chuyển ảnh
-function moveImages() {
-	// Di chuyển các ảnh xuống dưới
-	leftPane.style.top = `${parseInt(leftPane.style.top || 0) + speed}px`;
-	centerPane.style.top = `${parseInt(centerPane.style.top || 0) + speed}px`;
-	rightPane.style.top = `${parseInt(rightPane.style.top || 0) + speed}px`;
-
-	// Kiểm tra nếu ảnh đã di chuyển ra khỏi vùng hiển thị
-	if (parseInt(leftPane.style.top) >= window.innerHeight) {
-		leftPane.style.top = `-${leftPane.offsetHeight - gap}px`; // Di chuyển ảnh lên trên
-	}
-	if (parseInt(centerPane.style.top) >= window.innerHeight) {
-		centerPane.style.top = `-${centerPane.offsetHeight - gap}px`; // Di chuyển ảnh lên trên
-	}
-	if (parseInt(rightPane.style.top) >= window.innerHeight) {
-		rightPane.style.top = `-${rightPane.offsetHeight - gap}px`; // Di chuyển ảnh lên trên
-	}
-}
-
-// Thiết lập hàm setInterval để gọi hàm moveImages mỗi 10ms
-setInterval(moveImages, 1000);
