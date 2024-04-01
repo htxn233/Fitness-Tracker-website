15fd8d8973baf192716e85e84dc1dd83c4c34a0f
cuziycloud <tranmyvan6157@gmail.com>
2024-04-01 13:00:34 +0700

    1st web interface change

--- contact_process.php
+++ contact_process.php
@@ -1,7 +1,7 @@
 <?php
 
     $to = "rockybd1995@gmail.com";
-    $from = $_REQUEST['email'];
+    $from = $_REQUEST['email']; // lay du lieu dua tren id (line 144-170)
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
     $number = $_REQUEST['number'];
@@ -12,9 +12,7 @@
 	$headers .= "Reply-To: ". $from . "\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
     $subject = "You have a message from your Bitmap Photography.";
-
     $logo = 'img/logo.png';
     $link = '#';
 
